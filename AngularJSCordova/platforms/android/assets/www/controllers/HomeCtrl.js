2a62ae8510ae5e654f71e845d983d9defea2f3f3
Tiago de Mello Britto <obritto@hotmail.com>
2015-05-30 10:33:22 -0300

    Cordova Login e ANgularJS

--- AngularJSCordova/platforms/android/assets/www/controllers/HomeCtrl.js
+++ AngularJSCordova/platforms/android/assets/www/controllers/HomeCtrl.js
@@ -1,3 +1,27 @@
-﻿app.controller("HomeCtrl", function ($scope) {
-    $scope.msg = "tela inicial";
-});
+﻿app.controller("HomeCtrl", ['$scope', 'authService', function ($scope, authService) {
+
+    $scope.loginData = {
+        userName: "",
+        password: ""
+    };
+
+    $scope.message = "";
+
+
+
+    $scope.loginUser = function () {
+
+        $scope.message = 'autenticando...';
+
+        authService.login($scope.loginData).then(function (response) {
+
+            $scope.message = 'login OK!';
+
+        }).catch(function (err) {
+           
+            $scope.message = 'Dificuldade ao Autenticar';
+
+
+        });
+    };
+}]);
---
+++ AngularJSCordova/www/services/authService.js
@@ -0,0 +1,50 @@
+﻿app.factory('authService', ['$http', '$q',
+    function ($http, $q) {
+
+                  
+        var authServiceFactory = {};
+
+
+        var _login = function (loginData) {
+
+            var deferred = $q.defer();
+
+            var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password + "  ";
+
+
+            $http.post('http://192.168.0.28:85/token',
+                data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+                .success(function (response) {
+          
+
+                // grava token
+                //localStorage.setItem('authorizationData', JSON.stringify({ token: response.access_token, userName: loginData.userName }));
+
+                //_authentication.isAuth = true;
+                //_authentication.userName = loginData.userName;
+
+
+
+                //// seta o token
+                //_setAutorization(response.access_token);
+
+
+                deferred.resolve(response);
+
+            }).error(function (err, status) {
+
+                var e = angular.fromJson(err);
+
+                deferred.reject(e);
+            });
+
+            return deferred.promise;
+
+        };
+
+  
+
+        authServiceFactory.login = _login;
+
+        return authServiceFactory;
+}]);
